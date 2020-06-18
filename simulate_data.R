2cb1397f4cf9b45b82ff17c43071da1d8a631791
Daniel Modin <danielmodin.md@gmail.com>
2020-06-18 16:11:31 +0200

    Omdannet kode til funktion der kan tage de nødv parametre

--- simulate_data.R
+++ simulate_data.R
@@ -1,53 +1,76 @@
-#time grid 
-time.grid <- seq(0,20,1)
-NT = length(time.grid)
-#sample size 
-sample.size = 10 
 
-#exposure duration
-exp.duration = 2
+################ DEFINE FUNCTIONS #######################
 
-#pre exposure 
-X = rlnorm(n=sample.size,mean=2,sd=0.5)
+#expit function
+expit <- function(x) { exp(x)/(1+exp(x)) }
 
-#odds ratio exposure
-oddsratio.exp = 1 
-
-#oddsratioo for comoorbs
-oddsrato.X = 1.05
-
-#baseline risk 
-odds.Y = 1/1000
-
-#data matrix 
-E = matrix(numeric(sample.size*NT),ncol=NT)
-Y = matrix(numeric(sample.size*NT),ncol=NT)
-
-#prob exposure
-exp.prob = 0.1
-
-#outcome model 
-lin.pred = log(oddsrato.X)*X + log(odds.Y)
-
-for (j in time.grid) {
-  if (j >= exp.duration) {
-    print (j) 
-    E[,j+1] <- rbinom(sample.size,size=1,exp.prob)
-    has.exposure = rowSums(E)
-    E[has.exposure == 1, j+1] <- 0 #reset exspoure to 0 for previously exposed
+#simulate data function 
+sim <- function(time.grid.lower,  time.grid.upper,  time.step ,  sample.size,  exposure.duration,  frailty.mean,  frailty.sd ,  oddsratio.exposure,  oddsratio.X,  odds.baseline,  exposure.probability ) {
+  
+  #create time grid 
+  time.grid <- seq(time.grid.lower,time.grid.upper,  time.step)
+  time.grid.length = length(time.grid)
+  
+  #lav pre-study frailty 
+  X = rlnorm(n= sample.size,  mean = frailty.mean,  sd = frailty.sd)
+  
+  #data matrix initialization
+  E = matrix( numeric(sample.size*time.grid.length),  ncol = time.grid.length)
+  Y = matrix( numeric(sample.size*time.grid.length),  ncol = time.grid.length)
+  
+  #linear prediction based on frailty and baseline risk 
+  lin.pred = log(oddsratio.X)*X + log(odds.baseline)
+  
+  #simulate data
+  for (j in time.grid) { 
+    if (j>= exposure.duration) {
+     print(j)
+      
+     #draw exposure 
+     E[,j+1] <- rbinom(sample.size,  size=1, exposure.probability)
+     
+     #reset exposure to 0 for already exposed
+     has.exposure = rowSums(E)
+     E[has.exposure == 1, j+1] <- 0 #reset exp to 0 for prev exposed
+     
+     #update linear predictor ift exposed or not
+     current.exposure = rowSums(E[,(j+1-exposure.duration):(j+1)])
+     current.lin.pred = lin.pred + log(oddsratio.exposure)*current.exposure
     
-    current.exp = rowSums(E[,(j+1-exp.duration):(j+1)])
-    
-    current.lin.pred = lin.pred + log(oddsratio.exp)*current.exp
-    
-    expit <- function(x) { exp(x)/(1+exp(x)) }
-    
-    Y[,j+1] <- rbinom(sample.size,size=1,expit(current.lin.pred))
-    
+     #update outcomes 
+     Y[,j+1] <- rbinom(sample.size,size=1,expit(current.lin.pred))
+    }
   }
+  
+  #plot pt frailty distribution
+  hist(X)
+  
+  #return data in a list with E and Y 
+  return.list = list("E"=E, "Y"=Y)
+  return(return.list) 
 }
 
-hist(X)
+
+################ RUN SIMULATIONS  #######################
+
+#run sim 1
+sim1 = sim( 0,   #time.grid.lower
+        40,      #time.grid.upper
+         1,      #time.step
+         1000,   #sample.size
+         2,      #exposure.duration
+         2,      #frailty.mean 
+         0.5,    #frailty.sd
+         1,      #oddsratio.exposure
+         1.05,   #oddsratio.X (odds associeret med comorbs/frailty)
+         0.001,  #odds.baseline aka baseline risk 
+         0.1     #exposure.probability 
+)
+
+
+
+
+
 
 
 
