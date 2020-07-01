1558b95ce7dc3fb06c8832ce6d08ee8f01d9ae22
Daniel Modin <danielmodin.md@gmail.com>
2020-07-01 18:56:46 +0200

    Observed exposure datasæt (E.observed) kode tilføjet. OR forbundet med komorbiditet + baseline probability of exposure funktionalitet

--- simulate_data.R
+++ simulate_data.R
@@ -5,7 +5,7 @@
 expit <- function(x) { exp(x)/(1+exp(x)) }
 
 #simulate data function 
-sim <- function(time.grid.lower,  time.grid.upper,  time.step ,  sample.size,  exposure.duration,  frailty.mean,  frailty.sd ,  oddsratio.exposure,  oddsratio.X,  odds.baseline,  exposure.probability ) {
+sim <- function(time.grid.lower,  time.grid.upper,  time.step ,  sample.size,  exposure.duration,  frailty.mean,  frailty.sd ,  oddsratio.exposure,  oddsratio.X,  odds.baseline,  exposure.probability, baseline.probability.observation, oddsratio.observation ) {
   
   #create time grid 
   time.grid <- seq(time.grid.lower,time.grid.upper,  time.step)
@@ -17,14 +17,23 @@ sim <- function(time.grid.lower,  time.grid.upper,  time.step ,  sample.size,  e
   #data matrix initialization
   E = matrix( numeric(sample.size*time.grid.length),  ncol = time.grid.length)
   Y = matrix( numeric(sample.size*time.grid.length),  ncol = time.grid.length)
+  E.observed = matrix( numeric(sample.size*time.grid.length),  ncol = time.grid.length)
   
-  #linear prediction based on frailty and baseline risk 
+  #outcome linear prediction based on frailty and baseline risk 
   b_0 = log(odds.baseline)
   b_1 = log(oddsratio.X)
-  lin.pred = b_0 + b_1*X              
+  lin.pred = b_0 + b_1*X     
   
   #exposure coefficient
   b_exposure = log(oddsratio.exposure)
+  
+  
+  #exposure observation probability
+  b_baseline = log(baseline.probability.observation/(1-baseline.probability.observation))
+  b_observation = log(oddsratio.observation)
+  lin.pred.observation =b_baseline + b_observation*X
+  observation.probability = expit(lin.pred.observation)
+
   
   #simulate data
   for (j in time.grid) { 
@@ -47,6 +56,10 @@ sim <- function(time.grid.lower,  time.grid.upper,  time.step ,  sample.size,  e
     
      #update outcomes 
      Y[,j+1] <- rbinom(sample.size,size=1, outcome.probability)
+     
+     #draw observed exposure
+     has.current.exposure = E[,j] #logically, 1 if has and 0 if does not have
+     E.observed[has.current.exposure == 1, j] <- rbinom( sum(has.current.exposure) , size=1, observation.probability[has.current.exposure==1] )  #draw observed exposure with probability modified by frailty / comorbidity status
     }
   }
   
@@ -54,7 +67,7 @@ sim <- function(time.grid.lower,  time.grid.upper,  time.step ,  sample.size,  e
   hist(X)
   
   #return data in a list with E and Y 
-  return.list = list("E"=E, "Y"=Y)
+  return.list = list("E"=E, "E.observed" = E.observed, "Y"=Y)
   return(return.list) 
 }
 
@@ -72,14 +85,20 @@ sim1 = sim( 0,      #time.grid.lower
             1,      #oddsratio.exposure
             1.05,   #oddsratio.X (odds ratio associeret med comorbs/frailty)
             0.001,  #odds.baseline aka baseline risk (når odds er lille er odds ca lig sandsynlighed)
-            0.01    #exposure.probability 
+            0.01,   #exposure.probability 
+            0.5,    #baseline probability of observing exposure given 0 effect of comorbidty (intercept term in log reg)
+            1       #oddsratio for exposure observation depending on comorbidity/frailty
 )
 
 E = sim1$E
 Y = sim1$Y
+E.observed = sim1$E.observed
 
 print(sum(Y)) #check hvor mange outcomes
 print(sum(E)) #check hvor mange exposure 
+print(sum(E.observed)) #check hvor mange observed exposures 
+
+
 
 
 
